038f73e537235580a91579cd7657f6aa051be7ec
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-24 12:07:42 -0500

    update Header.js
    finished header section

--- linkedin-clone-front/src/App.js
+++ linkedin-clone-front/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      {<Header /> }
-      {/* {App Body} */}
-      {/* {Sidebar} */}
+      <Header /> 
+      <div className="app_body">
+        <Sidebar />
       {/* {Feed} */}
       {/* {Widget} */}
+      </div>
     </div>
   );
 }
--- linkedin-clone-front/src/Header.js
+++ linkedin-clone-front/src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
   return (
@@ -18,11 +21,11 @@ function Header() {
         <div className="header__right">
             <HeaderOption title='Home' Icon={HomeIcon}/>
             <HeaderOption title='My Network' Icon={SupervisorAccountIcon} />
-            <HeaderOption title='Jobs' />
-            <HeaderOption title='Messaging' />
-            <HeaderOption title='Notifications' />
-            <HeaderOption title='Me' />
-
+            <HeaderOption title='Jobs' Icon={BusinessCenterIcon}/>
+            <HeaderOption title='Messaging' Icon={ChatIcon}/>
+            <HeaderOption title='Notifications' Icon={NotificationsIcon}/>
+            
+            <HeaderOption title='me' avatar="https://media-exp1.licdn.com/dms/image/C5603AQGmMlXIi_5qZw/profile-displayphoto-shrink_800_800/0/1637535035189?e=1664409600&v=beta&t=KH_LjbEKDbMDQRPJ1PcTlLrl7lBT5recl1XnzLdnelk"/>
 
         </div>
     </div>
